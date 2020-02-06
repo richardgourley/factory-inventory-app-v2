fba407797632e5fb14771aa36a3e97c93676ca70
unknown <codeleap@outlook.com>
2020-02-06 15:05:32 +0100

    New product and user routes added to main menu.

--- views/main.php
+++ views/main.php
@@ -12,10 +12,13 @@
       <p>PRODUCTS:</p>
       <li><a href="<?php echo SITE_URL . '/product/index'; ?>">View all products</a></li>
       <li><a href="<?php echo SITE_URL . '/product/add'; ?>">Add a product</a></li>
+      <li><a href="<?php echo SITE_URL . '/product/edit'; ?>">Edit a product</a></li>
+      <li><a href="<?php echo SITE_URL . '/product/delete'; ?>">Delete a product</a></li>
       <p>USERS:</p>
       <li><a href="<?php echo SITE_URL . '/user/index'; ?>">View all users</a></li>
       <li><a href="<?php echo SITE_URL . '/user/add'; ?>">Add a user</a></li>
-      <li>Hello again</li>
+      <li><a href="<?php echo SITE_URL . '/user/edit'; ?>">Edit a user</a></li>
+      <li><a href="<?php echo SITE_URL . '/user/delete'; ?>">Delete a user</a></li>
     </ul>
     <?php require_once( $view ); ?>
   </body>
