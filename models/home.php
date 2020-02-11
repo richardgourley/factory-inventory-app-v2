63093ee9f0cd4a01aa2397334ab8ab7998d078c4
unknown <codeleap@outlook.com>
2020-02-11 07:08:54 +0100

    Index method left blank as no results required.

--- models/home.php
+++ models/home.php
@@ -2,12 +2,7 @@
 class HomeModel extends Model{
     
     public function Index(){
-    	/*$conn = $this->connect();
-    	$query = "SELECT * FROM products";
-    	$handle = $conn->prepare( $query );
-    	$handle->execute();
-    	$results = $handle->fetchAll( PDO::FETCH_ASSOC ); 
-    	return $results;*/
+    	
         return '';
     }
 
