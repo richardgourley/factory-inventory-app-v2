301441b38a82fe04cd0dfd6d1bb83aea482c5c06
unknown <codeleap@outlook.com>
2020-02-12 17:03:56 +0100

    Logic added to produce message if $viewmodel count = 0.

--- views/product/index.php
+++ views/product/index.php
@@ -1,5 +1,10 @@
 <h1>Products</h1>
 
+<?php if( count( $viewmodel ) == 0 ): ?>
+<h3>There are currently no products to view</h3>
+<?php endif; ?>
+<?php return; ?>
+
 <?php foreach( $viewmodel as $product ): ?>
 <div>
   <h3><?php echo $product['product_name']; ?></h3>
