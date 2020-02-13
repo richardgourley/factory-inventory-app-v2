3c486677154e458f9fb01e8ffa24e342ffb12d98
unknown <codeleap@outlook.com>
2020-02-13 14:44:40 +0100

    Stylesheet added. Grid containers added.

--- views/menus/logInMenu.php
+++ views/menus/logInMenu.php
@@ -3,16 +3,21 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <head>
   	<title>Factory Inventory App</title>
-  	<link rel="stylesheet" href="">
+    <link rel="stylesheet" href="<?php echo SITE_URL . '/assets/styles.css' ?>">
   </head>
   <body>
-    <h1>Factory Inventory App</h1>
+    <h1 class="title">Factory Inventory App</h1>
+    <div class="grid-container">
+    <div class="menu"> 
     <ul>
       <li><a href="<?php echo SITE_URL; ?>">Home</a></li>
       <br>
       <li><a href="<?php echo SITE_URL . '/user/logIn'; ?>">Log In</a></li>
     </ul>
+    </div>
+    <div class="view-display">
     <?php require_once( $view ); ?>
+    </div>
   </body>
 </html>
 
