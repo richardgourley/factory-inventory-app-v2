7f381049a1200e8e769a1449f9866b0d54f88dc0
unknown <codeleap@outlook.com>
2020-02-13 14:43:18 +0100

    Grid container added. Css file added.

--- views/menus/fullPrivMenu.php
+++ views/menus/fullPrivMenu.php
@@ -3,10 +3,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <head>
   	<title>Factory Inventory App</title>
-  	<link rel="stylesheet" href="">
+  	<link rel="stylesheet" href="<?php echo SITE_URL . '/assets/styles.css' ?>">
   </head>
   <body>
-    <h1>Factory Inventory App</h1>
+    <h1 class="title">Factory Inventory App</h1>
+    <div class="grid-container">
+    <div class="menu">  
     <ul>
       <li><a href="<?php echo SITE_URL; ?>">Home</a></li>
       <p>PRODUCTS:</p>
@@ -22,7 +24,11 @@
       <br>
       <li><a href="<?php echo SITE_URL . '/user/logOut'; ?>">Log Out</a></li>
     </ul>
+    </div>
+    <div class="view-display">
     <?php require_once( $view ); ?>
+    </div>
+    </div>
   </body>
 </html>
 
